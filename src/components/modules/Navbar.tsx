e705d94893bb390056297d5728cec58c4677aec8
Milad salamian <miladsalami1385@gmail.com>
2025-01-28 07:47:10 -0800

    render menu dynamicly with an test data

--- src/components/modules/Navbar.tsx
+++ src/components/modules/Navbar.tsx
@@ -2,17 +2,20 @@ import Link from "next/link";
 import { HiOutlineShoppingCart } from "react-icons/hi";
 
 function Navbar() {
+  const navLinks = [
+    { id: 1, href: "/", label: "Home" },
+    { id: 2, href: "/cart", label: "cart" },
+  ];
   return (
     <div className="shadow-md flex items-center justify-between px-6 py-5">
       <div className="flex items-center gap-x-10">
         <h1 className="text-lg">Milad Shop</h1>
         <ul className="flex items-center gap-x-3">
-          <li>
-            <Link href={'/'}>Home</Link>
-          </li>
-          <li>
-            <Link href={'/cart'}>cart</Link>
-          </li>
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <Link href={link.href}>{link.label}</Link>
+            </li>
+          ))}
         </ul>
       </div>
       <Link href={`/cart`} className="block relative cursor-pointer">
